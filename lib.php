365e340436d398c041f0b523073e9b8c144d304f
marcus.green@tituslearning.com <marcusavgreen@gmail.com>
2022-10-19 20:32:47 +0100

    linting fix

--- lib.php
+++ lib.php
@@ -220,7 +220,7 @@ function update_availability(array $data, \stdClass $driprelease) {
                                 '0' => $availablestart,
                                 '1' => $availablend
                             ],
-                            'showc' => [false,false],
+                            'showc' => [false, false],
                         ];
 
                         $DB->set_field(
