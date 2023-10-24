4e243021b9052c610a411180626e26d598b4561d
Marcus Green <marcusavgreen@gmail.com>
2023-10-24 21:45:39 +0100

    Version bump and changelog update

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_driprelease';
-$plugin->release = '1.0.0';
-$plugin->version = 2023100300;
+$plugin->release = '1.0.1';
+$plugin->version = 2023102400;
 $plugin->requires = 2020110900;
 $plugin->maturity = MATURITY_BETA;
